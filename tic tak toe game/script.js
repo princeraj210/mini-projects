6b09dd11bd364943eb51dd7bf1fd8dd5732813df
Prince <princeraj210180@gmail.com>
2024-07-27 09:36:15 +0530

    html , css complete

---
+++ tic tak toe game/script.js
@@ -0,0 +1,58 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+//create a function to intialise the game
+
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["", "", "", "", "", "", "", "", ""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+initGame()
+
+function swapTurn() {
+    if (currentPlayer === "X") {
+        currentPlayer = "O";
+    }
+    else {
+        currentPlayer = "X";
+    }
+    //ui update
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+function handleClick(index) {
+    if (gameGrid[index] === "") {
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none";
+        //swap the turn
+        swapTurn();
+        //check that winner is not exixt yet
+        checkGameOver();
+    }
+}
+
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        handleClick(index);
+    })
+});
+
+newGameBtn.addEventListener("click", initGame);
